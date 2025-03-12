d6fda222c82671bc043ed3dd9c32174d0f973aa4
rndud008 <rndud008@gmail.com>
2025-03-13 02:00:46 +0900

    입문 3day 유클리드 호제법을 이용한 GCD, 스트림

---
+++ src/입문/배열의평균값.java
@@ -0,0 +1,27 @@
+package 입문;
+
+import java.util.Arrays;
+
+public class 배열의평균값 {
+    //정수 배열 numbers가 매개변수로 주어집니다.
+    // numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for (int num : numbers){
+            answer+= num;
+        }
+
+        return answer/ numbers.length;
+    }
+
+    public double solution2(int[] numbers) {
+        return Arrays.stream(numbers).average().orElse(0);
+    }
+
+    public double solution3(int[] numbers) {
+        return Arrays.stream(numbers).reduce(0,Integer::sum) / (double) numbers.length;
+    }
+
+
+}
---
+++ src/입문/옷가게할인받기.java
@@ -0,0 +1,22 @@
+package 입문;
+
+public class 옷가게할인받기 {
+    // 머쓱이네 옷가게는 10만 원 이상 사면 5%,
+    // 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+    //구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+    public int solution(int price) {
+        int answer = price;
+
+        if (price >= 500_000){
+            answer = (int) ( answer * 0.8);
+        } else if (price >= 300_000) {
+            answer = (int) ( answer * 0.9);
+        } else if(price >= 100_000){
+            answer = (int) ( answer * 0.95);
+        }
+
+        return answer;
+    }
+
+}
