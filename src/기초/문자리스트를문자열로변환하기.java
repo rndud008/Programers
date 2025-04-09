80e17b7f7e26377f7eb463e80e9af3c5f4d716bc
rndud008 <rndud008@gmail.com>
2025-04-10 01:33:26 +0900

    기초 2day

---
+++ src/기초/문자리스트를문자열로변환하기.java
@@ -0,0 +1,11 @@
+package 기초;
+
+public class 문자리스트를문자열로변환하기 {
+    // 문자들이 담겨있는 배열 arr가 주어집니다.
+    // arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+
+    public String solution(String[] arr) {
+        String answer = String.join("",arr);
+        return String.join("",arr);
+    }
+}
---
+++ src/기초/홀짝에따라다른값반환하기.java
@@ -0,0 +1,56 @@
+package 기초;
+
+import java.util.stream.IntStream;
+
+public class 홀짝에따라다른값반환하기 {
+    // 양의 정수 n이 매개변수로 주어질 때,
+    // n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고
+    // n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+
+    public int solution(int n) {
+        int answer = 0;
+        if (n % 2 == 0){
+            for (int i = 0; i <= n; i+=2) {
+                answer += (i *i);
+            }
+        }else {
+            for (int i = 1; i <= n; i+=2) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+
+    public int solution2(int n) {
+        int answer = 0;
+        for (int i = (n % 2 == 0 ? 0 : 1); i <= n; i += 2) {
+            answer += (n % 2 == 0 ? i * i : i);
+        }
+        return answer;
+    }
+
+    public int solution3(int n) {
+        if (n % 2 == 0) {
+            int answer = 0;
+            for (int i = 0; i <= n; i += 2) {
+                answer += i * i;
+            }
+            return answer;
+        } else {
+            int k = (n + 1) / 2; // 홀수 개수
+            return k * k; // 1 + 3 + 5 + ... + n = k^2
+        }
+    }
+    public int solution4(int n) {
+        if (n % 2 == 0) {
+            return IntStream.rangeClosed(0, n)
+                    .filter(i -> i % 2 == 0)
+                    .map(i -> i * i)
+                    .sum();
+        } else {
+            return IntStream.rangeClosed(1, n)
+                    .filter(i -> i % 2 != 0)
+                    .sum();
+        }
+    }
+}
