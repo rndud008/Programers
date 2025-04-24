c8b0c18a990821f348f7117109ad5b000a129dc9
rndud008 <rndud008@gmail.com>
2025-04-25 00:15:10 +0900

    기초 13day

---
+++ src/기초/배열에서문자열대소문자변환하기.java
@@ -0,0 +1,19 @@
+package 기초;
+
+import java.util.Arrays;
+
+public class 배열에서문자열대소문자변환하기 {
+    // 문자열 배열 strArr가 주어집니다.
+    // 모든 원소가 알파벳으로만 이루어져 있을 때,
+    // 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로,
+    // 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
+
+    public String[] solution(String[] strArr) {
+        String[] answer = Arrays.copyOf(strArr,strArr.length);
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = i %2 == 0?  answer[i].toLowerCase() : answer[i].toUpperCase();
+        }
+        return answer;
+    }
+}
