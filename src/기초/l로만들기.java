7eb79efaa903e8d5cbcc4397a88938d684664a23
rndud008 <rndud008@gmail.com>
2025-05-03 22:01:09 +0900

    기초 19day

---
+++ src/기초/l로만들기.java
@@ -0,0 +1,37 @@
+package 기초;
+
+import java.util.stream.Collectors;
+
+public class l로만들기 {
+    // 알파벳 소문자로 이루어진 문자열 myString이 주어집니다.
+    // 알파벳 순서에서 "l"보다 앞서는 모든 문자를
+    // "l"로 바꾼 문자열을 return 하는 solution 함수를 완성해 주세요.
+
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+        for (char c : myString.toCharArray()){
+
+            answer.append(c > 'l' ? c : 'l');
+
+        }
+        return answer.toString();
+    }
+
+    public String solution2(String myString) {
+        String result = myString.chars()
+                .mapToObj(c -> (char)c > 'l' ? String.valueOf((char)c) : "l")
+                .collect(Collectors.joining());
+        return result;
+    }
+
+    public String solution3(String myString) {
+        char[] chars = myString.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] <= 'l') {
+                chars[i] = 'l';
+            }
+        }
+        return new String(chars);
+    }
+
+}
---
+++ src/기초/그림확대.java
@@ -0,0 +1,30 @@
+package 기초;
+
+import java.util.Arrays;
+
+public class 그림확대 {
+    // 직사각형 형태의 그림 파일이 있고,
+    // 이 그림 파일은 1 × 1 크기의
+    // 정사각형 크기의 픽셀로 이루어져 있습니다.
+    // 이 그림 파일을 나타낸 문자열 배열 picture과
+    // 정수 k가 매개변수로 주어질 때,
+    // 이 그림 파일을 가로 세로로 k배
+    // 늘린 그림 파일을 나타내도록
+    // 문자열 배열을 return 하는 solution 함수를 작성해 주세요
+
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        for (int i = 0; i < picture.length; i++) {
+            StringBuilder builder = new StringBuilder();
+            for (char c : picture[i].toCharArray()){
+                builder.append(String.valueOf(c).repeat(k));
+            }
+
+            for (int j = 0; j < k; j++) { // 세로 확대
+                answer[i * k + j] = builder.toString();
+            }
+
+        }
+        return answer;
+    }
+}
