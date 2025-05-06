8578cc9b42b185d607584141815a9849ef0b1d2b
rndud008 <rndud008@gmail.com>
2025-05-06 23:35:59 +0900

    PCCE 기출 8번, 1~4 문제

---
+++ src/level0/pcce/PCCE기출문제2번피타고라스의정리.java
@@ -0,0 +1,23 @@
+package level0.pcce;
+
+import java.util.Scanner;
+
+public class PCCE기출문제2번피타고라스의정리 {
+
+    // 직각삼각형이 주어졌을 때 빗변의 제곱은
+    // 다른 두 변을 각각 제곱한 것의 합과 같습니다.
+    // 직각삼각형의 한 변의 길이를 나타내는 정수 a와
+    // 빗변의 길이를 나타내는 정수 c가 주어질 때,
+    // 다른 한 변의 길이의 제곱,
+    // b_square 을 출력하도록 한 줄을 수정해 코드를 완성해 주세요.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = (int)Math.sqrt((c*c) - (a*a));
+
+        System.out.println(b_square);
+    }
+}
